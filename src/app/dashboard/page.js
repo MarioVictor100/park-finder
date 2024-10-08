3c7aa7461f31cc4b833880907056ae1134351b24
MARIO VICTOR ALVES LOPES <MARIO0644@DIGITALCOLLEGE.LOCAL>
2024-10-07 21:53:21 -0300

    primeria versão

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,8 @@
+export default function DashBoardPage() {
+    return (
+        <div className="flex relative min-h-full items-center flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="absolute left-0 min-h-screen min-w-24 p-2 flex justify-center bg-blue-950 text-white">
+                A
+            </div>
+        </div>)
+}
